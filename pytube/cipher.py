0c4775668d25989d235bf54e196b3a29cfb19eff
Reason Duan <duan_huanlong@163.com>
2023-09-28 11:15:27 +0800

    fix not march

--- pytube/cipher.py
+++ pytube/cipher.py
@@ -408,7 +408,10 @@ def get_throttling_plan(js: str):
     plan_regex = re.compile(transform_start)
     match = plan_regex.search(raw_code)
 
-    transform_plan_raw = find_object_from_startpoint(raw_code, match.span()[1] - 1)
+    # Reason, If there is no match, use the original data 
+    transform_plan_raw = js
+    if match :
+        transform_plan_raw = find_object_from_startpoint(raw_code, match.span()[1] - 1)
 
     # Steps are either c[x](c[y]) or c[x](c[y],c[z])
     step_start = r"c\[(\d+)\]\(c\[(\d+)\](,c(\[(\d+)\]))?\)"
